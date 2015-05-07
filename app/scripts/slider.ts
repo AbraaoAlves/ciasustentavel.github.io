471fd542b1bc1c79029d1745b7af4e4adcb6c3a2
abraaoalves <abraao.teodosio@gmail.com>
2015-05-07 18:20:32 -0300

    insert profile images

--- app/scripts/slider.ts
+++ app/scripts/slider.ts
@@ -1,11 +1,11 @@
-/// <reference path="../../typings/flexSlider/flexSlider.d.ts" />
+/// <reference path="../typings/flexSlider/flexSlider.d.ts" />
 
 (function(){
 	"use strict";
 	
 	$(".flexslider").flexslider({
 		animation:"fade",
-		animationLoop: true,
+		//animationLoop: false,
 		minItems: 2,
 		maxItems: 3
 	});
--- tasks/typescript.task.js
+++ tasks/typescript.task.js
@@ -13,29 +13,29 @@ gulp.task('lib-compile', function(){
      return gulp.src([
          'app/scripts/libs/jquery.flexslider.js'
       ])
-      .pipe(changed('app/scripts/', {extension: '.js'}))
-      .pipe(gulpIf(deploy, sourcemaps.init()))
+      .pipe(changed('app/scripts/js', {extension: '.js'}))
+      .pipe(gulpIf(!deploy, sourcemaps.init()))
       .pipe(concat('thirdparty.js'))
       .pipe(uglify()) 
-      .pipe(gulpIf(deploy, sourcemaps.write()))
-      .pipe(gulp.dest('app/scripts'));
+      .pipe(gulpIf(!deploy, sourcemaps.write()))
+      .pipe(gulp.dest('./app/scripts/js'));
 });
 
 gulp.task('script-compile:dev', ['lib-compile'], function() {
      var tsResult = gulp.src(['app/scripts/**/*.ts'])
-        .pipe(changed('app/scripts/', {extension: '.js'}))
-        .pipe(gulpIf(deploy, sourcemaps.init())) // This means sourcemaps will be generated 
+        .pipe(changed('app/scripts/js', {extension: '.js'}))
+        .pipe(gulpIf(!deploy, sourcemaps.init())) // This means sourcemaps will be generated 
         .pipe(typescript({
-             typescript:require('typescript'),
-             sortOutput: true,
+             typescript : require('typescript'),
+             sortOutput : true,
              removeComments :true
          }));
 
       return tsResult.js
          .pipe(concat('main.js')) // You can use other plugins that also support gulp-sourcemaps
          .pipe(uglify()) 
-         .pipe(gulpIf(deploy, sourcemaps.write())) // Now the sourcemaps are added to the .js file 
-         .pipe(gulp.dest('app/scripts'));
+         .pipe(gulpIf(!deploy, sourcemaps.write())) // Now the sourcemaps are added to the .js file 
+         .pipe(gulp.dest('./app/scripts/js'));
 });	
 
 gulp.task('script-compile', function (done) {
