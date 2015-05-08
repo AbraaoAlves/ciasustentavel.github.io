77d187e33dfa69b08bcb33b1089d55e0d5b86c7c
abraaoalves <abraao.teodosio@gmail.com>
2015-05-08 17:04:09 -0300

    deploy

---
+++ app/scripts/dialog.ts
@@ -0,0 +1,41 @@
+
+interface HTMLDialogElement extends HTMLDivElement{
+	show:(e?: MouseEvent | Element )=>void;
+	showModal:(e?: MouseEvent | Element )=>void;
+	close:(returnvalue?:DOMStringList | string)=>void;
+	open:boolean;
+}
+
+declare var dialogPolyfill:any;
+
+(function() {
+	"use strict";
+
+	var links = document.querySelectorAll(".depoimentos a.showModal");
+	var	dialog = <HTMLDialogElement>document.querySelector("dialog");
+	var	dialog_content = <HTMLElement>dialog.querySelector(".content");
+	
+	function showImage(e) {
+		var self = <HTMLElement>this;
+		e.preventDefault();
+		
+		var content = <HTMLElement>self.parentElement.querySelector(".content");
+		
+		dialog_content.innerHTML =  content.innerHTML;
+		dialog.showModal();
+	}
+	
+	dialog.querySelector("button.close").addEventListener("click", function() {
+		dialog_content.innerHTML = "";
+		dialog.close();
+	});
+	
+	var testdialog = <HTMLDialogElement>document.createElement("dialog");
+	testdialog.setAttribute("open", "");
+	
+	if (!testdialog.open) { 
+		dialogPolyfill.registerDialog(dialog); 
+	}
+	
+	for (var i = 0; i < links.length; i++) { links[i].addEventListener("click", showImage); }
+})();
--- app/scripts/js/main.js
+++ app/scripts/js/main.js
@@ -1,2 +1,53 @@
-!function(){"use strict";function e(){o.classList.remove("open"),c.classList.remove("open"),s.classList.remove("open")}function n(){o.classList.toggle("open"),c.classList.toggle("open"),s.classList.toggle("open"),s.classList.add("opened")}var t=document.querySelector.bind(document),s=t(".navdrawer-container"),o=document.body,c=t(".app-bar"),i=t(".menu"),a=t("main");a.addEventListener("click",e),i.addEventListener("click",n),s.addEventListener("click",function(n){("A"===n.target.nodeName||"LI"===n.target.nodeName)&&e()})}(),function(){"use strict"}();
-//# sourceMappingURL=main.js.map
+(function () {
+    "use strict";
+    var links = document.querySelectorAll(".depoimentos a.showModal");
+    var dialog = document.querySelector("dialog");
+    var dialog_content = dialog.querySelector(".content");
+    function showImage(e) {
+        var self = this;
+        e.preventDefault();
+        var content = self.parentElement.querySelector(".content");
+        dialog_content.innerHTML = content.innerHTML;
+        dialog.showModal();
+    }
+    dialog.querySelector("button.close").addEventListener("click", function () {
+        dialog_content.innerHTML = "";
+        dialog.close();
+    });
+    var testdialog = document.createElement("dialog");
+    testdialog.setAttribute("open", "");
+    if (!testdialog.open) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", showImage);
+    }
+})();
+
+(function () {
+    'use strict';
+    var querySelector = document.querySelector.bind(document);
+    var navdrawerContainer = querySelector('.navdrawer-container');
+    var body = document.body;
+    var appbarElement = querySelector('.app-bar');
+    var menuBtn = querySelector('.menu');
+    var main = querySelector('main');
+    function closeMenu() {
+        body.classList.remove('open');
+        appbarElement.classList.remove('open');
+        navdrawerContainer.classList.remove('open');
+    }
+    function toggleMenu() {
+        body.classList.toggle('open');
+        appbarElement.classList.toggle('open');
+        navdrawerContainer.classList.toggle('open');
+        navdrawerContainer.classList.add('opened');
+    }
+    main.addEventListener('click', closeMenu);
+    menuBtn.addEventListener('click', toggleMenu);
+    navdrawerContainer.addEventListener('click', function (event) {
+        if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+            closeMenu();
+        }
+    });
+})();
